acf3fdb7e79f5cfe01c82dabe17815f6bd147fa2
Ubuntu_Work(Crystalmanner) <crystalmanner130@gmail.com>
2024-02-27 08:52:51 -0800

    disable inflation rate option

--- src/views/SpousalBenefitCalculator.vue
+++ src/views/SpousalBenefitCalculator.vue
@@ -29,8 +29,8 @@
               label="Lower Earner Full Retirement Age Benefit" type="text" prefix="$"
               hint="The full retirement age benefit the lower earning spouse earned from their own work"
               @blur="formatLowerEarnerBenefit" @input="stripLowerEarnerBenefitFormatting" dense></v-text-field>
-            <v-text-field v-model.number="inflationRate" :rules="[$numberGreaterThanNegativeRule, numberSmallerThan10]"
-              label="Inflation Rate" suffix="%" dense></v-text-field>
+            <!-- <v-text-field v-model.number="inflationRate" :rules="[$numberGreaterThanNegativeRule, numberSmallerThan10]"
+              label="Inflation Rate" suffix="%" dense></v-text-field> -->
             <v-checkbox v-model="meetDivorcedBenefit"
               label="Do you meet the qualifications for a divorced spouse benefit?"></v-checkbox>
           </v-form>
@@ -141,7 +141,7 @@ export default {
       ],
       lowerEarnerPaymentTitle: "",
       spousalExcessTitle: "",
-      inflationRate: 1,
+      inflationRate: 0,
       fullRetireMonths: 0,
     };
   },
